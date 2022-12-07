856d797dbde5145c97bea251d67b2b5e85455168
Irha Ali <irhaali_97@hotmail.ca>
2022-12-06 19:40:19 -0500

    implemented spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,6 @@
+let positions = '|/-\\|/-\\|'
+for (let i = 0; i < positions.length; i++) {
+  setTimeout(() => { (i !== positions.length - 1) ?
+    process.stdout.write(`\r${positions[i]}`) : process.stdout.write(`\r${positions[i]} \n`);
+  }, 100 + i*200);
+}
