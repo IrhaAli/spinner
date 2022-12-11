17c9259f1db4d1e75d1bc141a9a1a6502c089ff3
Irha Ali <irhaali_97@hotmail.ca>
2022-12-11 12:15:21 -0500

    Added space between cursor and animation

--- spinner2.js
+++ spinner2.js
@@ -1,6 +1,11 @@
-let positions = '|/-\\|/-\\|'
-for (let i = 0; i < positions.length; i++) {
-  setTimeout(() => { (i !== positions.length - 1) ?
-    process.stdout.write(`\r${positions[i]}`) : process.stdout.write(`\r${positions[i]} \n`);
-  }, 100 + i*200);
-}
+const spinner2 = function () {
+  let positions = '|/-\\|/-\\|'
+  for (let i = 0; i < positions.length; i++) {
+    setTimeout(() => {
+      (i !== positions.length - 1) ?
+      process.stdout.write(`\r${positions[i]}    `) : process.stdout.write(`\r${positions[i]} \n`);
+    }, 100 + i * 200);
+  }
+};
+
+spinner2();
