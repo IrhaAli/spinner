fd8f22c6009a9b31fc487fc8c6be023d62fb71cc
Irha Ali <irhaali_97@hotmail.ca>
2022-12-19 17:36:14 -0500

    Eslint

--- spinner1.js
+++ spinner1.js
@@ -12,12 +12,12 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r/   '); 
+  process.stdout.write('\r/   ');
 }, 900);
 
 setTimeout(() => {
--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,9 @@
-const spinner2 = function () {
-  let positions = '|/-\\|/-\\|'
+const spinner2 = function() {
+  let positions = '|/-\\|/-\\|';
   for (let i = 0; i < positions.length; i++) {
     setTimeout(() => {
       (i !== positions.length - 1) ?
-      process.stdout.write(`\r${positions[i]}    `) : process.stdout.write(`\r${positions[i]} \n`);
+        process.stdout.write(`\r${positions[i]}    `) : process.stdout.write(`\r${positions[i]} \n`);
     }, 100 + i * 200);
   }
 };
